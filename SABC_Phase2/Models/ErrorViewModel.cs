41b7ce6568c0dc51acdfa325f9b57e290b8bd2f8
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-06-09 11:14:53 +0200

    Push 1
    Created Project and pushed to github

---
+++ SABC_Phase2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SABC_Phase2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
