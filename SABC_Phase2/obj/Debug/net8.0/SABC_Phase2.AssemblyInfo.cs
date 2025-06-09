c3a5d46f07749c0253865bb181399f25aa4d72a8
Ubaid Shaik <104141996+Ubaidullah-Shaik@users.noreply.github.com>
2025-06-09 16:26:00 +0200

    proper commit

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dab2f4b0b3a7230fab6880b5745918441eea7a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf45fc1316e91be4cd411e632fdd15ae38c0cb91")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
