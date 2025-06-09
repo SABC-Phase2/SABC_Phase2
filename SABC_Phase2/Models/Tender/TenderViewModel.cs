4aaf5e9c622818931ae52b8f0965a4b398018cce
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-06-09 15:57:46 +0200

    Created Tender Admin Models in Tenders folder

---
+++ SABC_Phase2/Models/Tender/TenderViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SABC_Phase2.Models.Tender
+{
+    public class TenderViewModel
+    {
+
+    }
+}
--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380a77fd81156f7e90964a0c516ee4d7c0b5d1a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dab2f4b0b3a7230fab6880b5745918441eea7a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
