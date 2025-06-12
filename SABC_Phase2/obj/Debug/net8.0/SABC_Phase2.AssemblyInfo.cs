cf7fda64658c9013e68829041ca440838026cace
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-06-12 10:00:38 +0200

    Added Comments to some classes and controllers

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e74ebf95ebe1841d6f3a3824726316967951ea08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca7ed6f6e2b57c00ea8277281c82690ef2988d76")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
