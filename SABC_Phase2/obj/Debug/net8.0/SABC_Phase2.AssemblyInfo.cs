2d7c089bbf1606c6ee574cd83d4bebce07c78ac5
Ubaid Shaik <104141996+Ubaidullah-Shaik@users.noreply.github.com>
2025-06-12 09:18:19 +0200

    teast

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a5d46f07749c0253865bb181399f25aa4d72a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e4dba5bb066c13217994cfa8d99c83279fcdd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
