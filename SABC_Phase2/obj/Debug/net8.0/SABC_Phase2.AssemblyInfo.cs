a89459b4a2a415b0ec5a044715ce7196f4561d99
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-07-24 16:00:01 +0200

    Fixed Create Validation in Tender Admin

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e77d1120f25daf832d7cfb681a1501978e65ad8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e660cd6845918c35f407bcc7b797484ebcca3253")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
