fe6d508b085b4d2d96d80f2eadbe98649175fbd0
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-07-28 10:39:51 +0200

    My code push

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e660cd6845918c35f407bcc7b797484ebcca3253")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a89459b4a2a415b0ec5a044715ce7196f4561d99")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
