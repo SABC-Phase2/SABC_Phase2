ad005bd7ff2a0707dcf0c8a8aaf7db46cd9fabba
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-08-27 17:40:44 +0200

    Company Details Done

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+763261c41c81fad1f6b3031edbda213160c327be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c0fda1be7a5d9a6f837bc56c99da713b69ab5f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
