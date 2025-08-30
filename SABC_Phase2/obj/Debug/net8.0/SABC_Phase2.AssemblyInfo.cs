0ce302017e773db77ed145bbe878aff097d62e5b
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-08-30 17:07:07 +0200

    Most upto date

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43339d0be55eb2263e733022bbbd60ae48179487")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed171837a1477a54ec229c7dcfd13772eaa13447")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
