108fbe52e07a50a364fb35f46962bd45a1f0095b
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-09-03 14:56:07 +0200

    New Push

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05d3b4d9ceace20db28d64223a3392337ea54910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8154657a8503977e71c5aaf56a4e4319a66799e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
