b993ed82a02a20b8815e32b565ed21d2f1726b7a
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-09-15 10:38:35 +0200

    Can schedule a tender on current day now

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+787e61e3d8ef0a39e1f6520e01ef55485b3f591b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+177cb29220416a80805f3475609774b391a58b94")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
