bb808c77f212f5998c325ff621f80c94f57b4ead
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-09-16 17:08:23 +0200

    Worked on Super Admin features

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f9111136f64b92c29a208290db9377aefad7c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ebf2ca0ba450a1b40a13118910dd1c94835a8fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
