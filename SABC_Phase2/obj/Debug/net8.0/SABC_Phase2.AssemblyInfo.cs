282f7d82b7a4928e9b5e772638c599167e731ba7
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-09-19 22:06:04 +0200

    Account Deletion OVRS_User

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+effe14937677825d90fdb8f3950082d2114b96ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c955570356790f226dc2e9394e264d452f8914a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
