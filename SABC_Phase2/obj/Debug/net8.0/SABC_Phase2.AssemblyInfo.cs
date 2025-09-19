7c955570356790f226dc2e9394e264d452f8914a
Wicked-Spartan26 <tashlyng123@gmail.com>
2025-09-19 21:39:29 +0200

    OVRS View Profile push before enforcing validation for password

--- SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
+++ SABC_Phase2/obj/Debug/net8.0/SABC_Phase2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9003b7abca53057ba2453e9f8be15e17bbf8c373")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+effe14937677825d90fdb8f3950082d2114b96ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SABC_Phase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
